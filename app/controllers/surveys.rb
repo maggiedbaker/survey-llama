ad634cdb904438f2f26f35128375dea8318fc2fa
smokecode <smokecode@gmail.com>
2015-10-10 17:16:06 -0400

    merge fix, wildcard moved to bottom of routes

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -7,6 +7,24 @@ get '/surveys' do
   erb :'surveys/index'
 end
 
+get '/surveys/new' do
+  erb :'/surveys/new'
+end
+
+post '/surveys/new' do
+  user = User.find_by(username: session[:username])
+  new_survey = Survey.new(user_id: user.id, title: params[:title])
+  choice = new_survey.questions[0].choices.find_by(text: params[:choice])
+  ## modify this when implementing multi question surveys. questions should be many not just index 0
+  choice.selected += 1
+  user.surveys << new_survey
+  if @survey.save
+    redirect ('/surveys')
+  else
+    redirect ('/surveys/new')
+  end
+end
+
 get '/surveys/:name' do
   @survey = Survey.find(params[:name])
   if logged_in?
