fe2a9815440eefde379e037d7d54b7befa935f26
smokecode <smokecode@gmail.com>
2015-10-10 15:53:26 -0400

    survey creation complete

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -3,23 +3,18 @@ get '/surveys/new' do
 end
 
 post '/surveys/new' do
-  user = User.find_by(username: session[:username])
-  new_survey = Survey.new(user_id: user.id, title: params[:title])
-  choice = new_survey.questions[0].choices.find_by(text: params[:choice])
-  ## modify this when implementing multi question surveys. questions should be many not just index 0
-  choice.selected += 1
+  user = User.find_by(username: session[:user_id])
+  new_survey = Survey.create(user_id: user.id, title: params[:title])
+  question = Question.new(text: params[:question]) #modify logic here if mutliple questions
+  new_survey.questions << question
+  params[:choices].reject! { |key, choice| choice[:text] == "" }
+  params[:choices].values.each do |choice|
+    question.choices << Choice.new(text: choice[:text])
+  end
   user.surveys << new_survey
-  if @survey.save
+  if user.save
     redirect ('/surveys')
   else
     redirect ('/surveys/new')
   end
-end
-
-get '/' do
-  redirect('/surveys')
-end
-
-get '/surveys' do
-  erb :'/surveys/index'
 end
