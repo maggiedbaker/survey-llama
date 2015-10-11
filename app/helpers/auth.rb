d06d3efb5a3f3442b01b268655b6b0ed8cec1f51
Maggie Baker <maggiedbaker@gmail.com>
2015-10-10 20:13:24 -0400

    fixed model logic

--- app/helpers/auth.rb
+++ app/helpers/auth.rb
@@ -1,7 +1,9 @@
-def logged_in?
-  !!session[:user_id]
-end
+ def logged_in?
+    !!session[:user_id]
+  end
 
-def current_user
-  @current_user ||= User.find(session[:user_id])
-end
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
+
+
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,5 @@
 class Survey < ActiveRecord::Base
+
   belongs_to :user
   has_many :questions
   has_many :completed_surveys
@@ -7,17 +8,6 @@ class Survey < ActiveRecord::Base
 
   def all_choices
     self.questions.first.choices.map { |choice| [choice.text, choice.selected] }
-  end
-
-  def current_user_can_take?
-    return false if !logged_in?
-    return false if self.current_user_has_taken?
-    return true
-  end
-
-  def current_user_has_taken?
-    return false if CompletedSurvey.where(user_id: current_user.id, survey_id: self.id).empty?
-    return true
   end
 
 end
