87db04cb5fbc3cb07093b58fc4590baac4d94d2d
smokecode <smokecode@gmail.com>
2015-10-11 03:34:09 -0400

    fixed image broken link and added a lot of css/jquery

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -2,9 +2,12 @@ class Choice < ActiveRecord::Base
   belongs_to :question
 
   validates :question_id, :text, :selected, presence: true
-
+  def ascii_all_day(percentage)
+    "="*(percentage/3)
+  end
   def percentage_chosen
     return 0 if self.question.times_answered == 0
-    ((self.selected.to_f / self.question.times_answered.to_f)*100).round
+    percentage = ((self.selected.to_f / self.question.times_answered.to_f)*100).round
+    "#{"."*(percentage/2.0)}#{percentage}"
   end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,39 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("#login_form").on('click', 'input[name=username]', function() {
+    if ($(this).val() == "Username")
+      $(this).val("");
+    else if ($(this).val() == "")
+      $(this).val("Username");
+  });
+  $("#login_form").on('click', 'input[name=password]', function() {
+    if ($(this).val() == "Password")
+      $(this).val("");
+    else if ($(this).val() == "")
+      $(this).val("Password");
+  });
+  $("#signup-username").click( function() {
+    if ($(this).val() == "Username")
+      $(this).val("");
+    else if ($(this).val() == "")
+      $(this).val("Username");
+  });
+  $("#signup-password").click( function() {
+    if ($(this).val() == "Password")
+      $(this).val("");
+    else if ($(this).val() == "")
+      $(this).val("Password");
+  });
+  $("#signup-confirm-password").click( function() {
+    if ($(this).val() == "Password")
+      $(this).val("");
+    else if ($(this).val() == "")
+      $(this).val("Password");
+  });
+  // not working just yet, need to catch right object
+  // $("#signup-password").keyup(function(){
+  //     if ($(this).find("#list_conf") === undefined)
+  //       $(this).append("<p id=\"confpw\">please confirm password</p>");
+  //     if ($(this).find("#list_conf").val() == "")
+  //       $(this).find("#list_conf").remove();
+  // });
 });
