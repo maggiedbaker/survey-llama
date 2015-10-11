58dd8f57d9cf8ec46a63c77dd9d542bd527fcdbf
smokecode <smokecode@gmail.com>
2015-10-10 21:03:47 -0400

    some changes to take survey

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -27,18 +27,18 @@ post '/surveys' do
   end
 end
 
-get '/surveys/:title' do
+get '/surveys/:title/results' do
   @survey = Survey.find_by(title: params[:title])
+  erb :'/surveys/results'
+end
+
+get '/surveys/:title' do
   if logged_in?
+    @survey = Survey.find_by(title: params[:title])
     erb :'/surveys/show'
   else
     redirect '/login'
   end
-end
-
-get '/surveys/:title/results' do
-  @survey = Survey.find_by(title: params[:title])
-  erb :'/surveys/results'
 end
 
 post '/surveys/:title' do
